78ca7e48971f8a187656a0072b731816dc47b15e
Hakan Sonmez <hakan.sn.hs@gmail.com>
2022-01-12 01:18:08 +0300

    Use AtomicU32 instead of Mutex<u32>

--- threadpool/src/worker.rs
+++ threadpool/src/worker.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, mpsc, Mutex};
+use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
 use std::thread;
 
 use crate::{Message};
@@ -6,7 +7,7 @@ use crate::{Message};
 pub struct Worker {
     pub id: usize,
     receiver: Arc<Mutex<mpsc::Receiver<Message>>>,
-    completed_jobs: Arc<Mutex<u32>>,
+    completed_jobs: Arc<AtomicU32>,
     pub(crate) thread: Option<thread::JoinHandle<()>>,
 }
 
@@ -15,13 +16,13 @@ impl Worker {
         Worker {
             id,
             receiver,
-            completed_jobs: Arc::new(Mutex::new(0)),
+            completed_jobs: Arc::new(AtomicU32::new(0)),
             thread: None,
         }
     }
 
     pub fn completed_task_count(&self) -> u32 {
-        *self.completed_jobs.lock().unwrap()
+        self.completed_jobs.load(Ordering::SeqCst)
     }
 
     pub fn start(&mut self) {
@@ -40,10 +41,10 @@ impl Worker {
                     Message::NewJob(job) => {
                         println!("Worker {} got a job; executing", id);
                         job();
-                        *job_count.lock().unwrap() += 1;
+                        job_count.fetch_add(1, Ordering::SeqCst);
                     },
                     Message::Terminate => {
-                        println!("Worker {} got terminate message. Total completed task count: {}", id, job_count.lock().unwrap());
+                        println!("Worker {} got terminate message. Total completed task count: {}", id, job_count.load(Ordering::SeqCst));
                         break;
                     }
                 }
